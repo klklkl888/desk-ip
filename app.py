7df3d1e8eb75589643fd3daaaff4cd1e6d264ace
klklkl888 <h9853473@gmail.com>
2024-10-16 15:54:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ app = Flask(__name__)
 LICENSED_IPS = [
     "192.168.0.67", 
     '112.196.47.98',# Replace this with the actual licensed IPs new era ip
-    '192.168.1.10'
+    '192.168.1.10',
+    '42.105.13.1'
 ]
 
 @app.route('/check_ip', methods=['GET'])
